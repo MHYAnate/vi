46b1629e553a0ed171abeffd1191379b680aef29
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-02-02 02:15:48 +0100

    fix:deployementIssues

--- vh/src/components/RagForm.tsx
+++ vh/src/components/RagForm.tsx
@@ -20,22 +20,7 @@ interface ContextItem {
 	similarity: number;
 }
 
-const IconSparkles = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
-		className="w-6 h-6 mr-2 text-yellow-500"
-	>
-		<path
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth={2}
-			d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-		/>
-	</svg>
-);
+
 
 export default function RagForm() {
 	const [response, setResponse] = useState<{
